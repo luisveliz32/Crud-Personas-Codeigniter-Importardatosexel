8d06ceb85b5dd374357ec032c688009c16c63f1e
luis veliz <luisveliz32@gmail.com>
2022-08-16 11:57:17 -0400

    Retorna solo la vista

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,9 +8,9 @@ class Home extends BaseController
 {
     public function index()
     {
-        $Persona=new Persona();
-        $datos['personas']=$Persona->where('estado', 1)->orderBy('id','ASC')->paginate(5);
-        return view('prueva',$datos);
+        //$Persona=new Persona();
+        //$datos['personas']=$Persona->where('estado', 1)->orderBy('id','ASC')->paginate(5);
+        return view('prueva');
     }
     
 
