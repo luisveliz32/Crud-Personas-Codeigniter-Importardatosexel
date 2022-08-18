fa349ff34f5109e7dbe0b1fc4f17585904beaed0
luis veliz <luisveliz32@gmail.com>
2022-08-17 20:17:47 -0400

    cambiado la url

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/dvsoft/public/';
+    public $baseURL = 'https://lmvc.herokuapp.com/';
 
     /**
      * --------------------------------------------------------------------------
